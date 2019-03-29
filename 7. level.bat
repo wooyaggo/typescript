844d6f3d361664c33a6fc37efb41b08a018e346f
jinsang.yun <jinsang.yun@linefriends.com>
2019-03-29 12:03:10 +0900

    Level Table

---
+++ 7. level.bat
@@ -0,0 +1,5 @@
+@echo off
+node bin 1v0LD8p26vMpteevz0tyoczhL7j5zP4gQdiKgFnQabGc level_exp D:/Works/BT21/unity/cooky/Assets/Resources/Data
+node bin 1v0LD8p26vMpteevz0tyoczhL7j5zP4gQdiKgFnQabGc level_stamina D:/Works/BT21/unity/cooky/Assets/Resources/Data
+node bin 1v0LD8p26vMpteevz0tyoczhL7j5zP4gQdiKgFnQabGc level_food D:/Works/BT21/unity/cooky/Assets/Resources/Data
+node bin 1v0LD8p26vMpteevz0tyoczhL7j5zP4gQdiKgFnQabGc level_train D:/Works/BT21/unity/cooky/Assets/Resources/Data
